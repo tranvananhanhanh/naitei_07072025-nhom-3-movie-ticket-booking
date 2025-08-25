bf9ce4463a55b391f9f1a2b29ce790b8f487d4a7
Thanh Tinh Nguyen <ngthanhtinh801@gmail.com>
2025-08-25 14:08:27 +0700

    fix use enum

--- src/main/java/com/naitei/group3/movie_ticket_booking_system/repository/ShowtimeRepository.java
+++ src/main/java/com/naitei/group3/movie_ticket_booking_system/repository/ShowtimeRepository.java
@@ -1,6 +1,7 @@
 package com.naitei.group3.movie_ticket_booking_system.repository;
 
 import com.naitei.group3.movie_ticket_booking_system.entity.Showtime;
+import com.naitei.group3.movie_ticket_booking_system.enums.ShowtimeStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -20,11 +21,12 @@ public interface ShowtimeRepository extends JpaRepository<Showtime, Long> {
                           AND (:cinemaName IS NULL OR LOWER(c.name) LIKE LOWER(CONCAT('%', :cinemaName, '%')))
                           AND (:status IS NULL OR s.status = :status)
                           AND (:showDate IS NULL OR FUNCTION('DATE', s.startTime) = :showDate)
+                        ORDER BY s.startTime desc
                         """)
         Page<Showtime> filterShowtimes(
                         @Param("keyword") String keyword,
                         @Param("cinemaName") String cinemaName,
-                        @Param("status") Integer status,
+                        @Param("status") ShowtimeStatus status,
                         @Param("showDate") LocalDate showDate,
                         Pageable pageable);
 }
